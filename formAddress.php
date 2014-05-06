c1e98a4767e3ff59dc12e7f1a2227384e89efdc6
vrowold <vrowold@gmail.com>
2014-05-06 12:17:03 -0400

    Added the html web form and the php code

---
+++ formAddress.php
@@ -0,0 +1,27 @@
+<?php
+//    mysql_connect("localhost","vrowold_bryant","tokeitupX1","vrowold_swisslist);
+
+    // echo "{{'address':". $_GET['address1']."},{'stars':". $_GET["rating1"]."}}";
+    //mysql_query("INSERT INTO parties VALUES ("123 e 15th","5")");
+    // mysql_query("INSERT INTO parties VALUES ($_GET["address1"],$_GET["rating1"])");
+//    mysql_query("INSERT INTO parties (address, stars, date_created,date_modified) VALUES ("123 e 15th","5")");
+
+    $con=mysqli_connect("localhost","vrowold_bryant","tokeitupX1","vrowold_swisslist");
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    $ad1 = $POST['address1'];
+    $sql="INSERT INTO parties (address, stars, date_created,date_modified)
+    VALUES ($ad1,4,NOW(),NOW())";
+
+    if (!mysqli_query($con,$sql))
+    {
+      die('Error: ' . mysqli_error($con));
+    }
+    echo "1 record added";
+
+    mysqli_close($con);
+?>
+
